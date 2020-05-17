5d330aafd4cde0eea21d38183bb28243a0b292c8
Nikolina Petrović <nikolinaapetrovic97@gmail.com>
2020-05-17 17:40:14 +0200

    Revert "model"
    This reverts commit 2ff01476251c925c9c0ab417d069d207275a2985.

--- Projekat/nutrition-adviser/src/main/java/sbnz/projekat/nutritionadviser/NutritionAdviserApplication.java
+++ Projekat/nutrition-adviser/src/main/java/sbnz/projekat/nutritionadviser/NutritionAdviserApplication.java
@@ -1,8 +1,5 @@
 package sbnz.projekat.nutritionadviser;
 
-import org.kie.api.KieServices;
-import org.kie.api.builder.KieScanner;
-import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,14 +9,5 @@ public class NutritionAdviserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NutritionAdviserApplication.class, args);
 	}
-	
-	
-	public KieContainer kieContainer() {
-		KieServices ks = KieServices.Factory.get();
-		KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("sbnz.projekat", "nutrition-adviser-drools", "0.0.1-SNAPSHOT"));
-		
-		KieScanner kScanner = ks.newKieScanner(kContainer);
-		kScanner.start(10_000);
-		return kContainer;
-	}
+
 }
