bf27cbeb58768ecbb2fb3f585fd3991e0eaf4b3b
Nikolina Petrović <nikolinaapetrovic97@gmail.com>
2020-05-17 19:40:20 +0200

    Newest recipies added to home page

--- Projekat/frontend/src/components/home/home-container.vue
+++ Projekat/frontend/src/components/home/home-container.vue
@@ -39,6 +39,42 @@
   <div class="container pt-5 pb-5">
     <h1>Najnoviji <span class="monsBold">recepti</span></h1>
   </div>
+  <div class="row newest">
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="../../../public/images/r3.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5>Hard Cider-Braised Pork </h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+        <br />
+        <span class="monsBold">11min</span> | Breakfast</p>
+        <a href="#" class="card-link">More details</a>
+
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="../../../public/images/r2.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5>Easy classic lasagne</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+        <br />
+        <span class="monsBold">23min</span> | Desert</p>
+        <a href="#" class="card-link">More details</a>
+
+      </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="../../../public/images/r1.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5>Thai fried prawn & pineapple rice</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+        <br />
+        <span class="monsBold">21min</span> | Lunch</p>
+        <a href="#" class="card-link">More details</a>
+
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 
