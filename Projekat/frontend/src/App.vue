cbc9c68ab9af6c3352b66ab66bc5594580fb7a92
Nikolina Petrović <nikolinaapetrovic97@gmail.com>
2020-05-17 17:48:04 +0200

    Edit profile implemented

---
+++ Projekat/frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from '@/components/navbar.vue'
+export default {
+  name: 'App',
+  components: {
+    'navbar': NavbarComponent
+  }
+}
+</script>
---
+++ Projekat/frontend/src/components/navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar navbar-inverse navigation navbar-fixed-top" style="background-color: #2d3e50;!important">
+   <div class="container">
+     <div class="navbar-header">
+       <h1 class="navbar-brand" href="#" v-on:click="redirect">|||Nutrition<span class="monsBold">Adviser</span></h1>
+     </div>
+   </div>
+ </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar-component',
+  methods: {
+    redirect() {
+        if (localStorage.getItem('token') == null) {
+          this.$router.push("/")
+      }
+    }
+
+  }
+}
+</script>
